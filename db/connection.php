b75f4d005f9f69c47c1b6f853f1cba0dca36a021
kuttaop123 <kartik.work11710@gmail.com>
2024-12-10 09:07:25 +0530

    connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'localhost'; // Server name
-$user = 'root';      // Database username
-$pass = '';          // Database password (leave empty for XAMPP)
-$db_name = 'db_sfms'; // Database name
+$host = 'localhost'; 
+$user = 'root';      
+$pass = '';          
+$db_name = 'db_sfms';
 
-// Create connection
+
 $conn = new mysqli($host, $user, $pass, $db_name);
 
-// Check connection
+
 if ($conn->connect_error) {
     die('Database connection failed: ' . $conn->connect_error);
 }
